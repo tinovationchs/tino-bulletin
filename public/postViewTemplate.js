265bb3de022cc5ab04e637ce39b5b36f4a8dd4c1
rk097 <78620631+rk097@users.noreply.github.com>
2023-02-12 15:30:02 -0800

    attachments fix and post fix
    @downloadablecontent

--- public/postViewTemplate.js
+++ public/postViewTemplate.js
@@ -1,7 +1,7 @@
 const post_template = `
 <div class="postBox">
     <h2 id="post-title"><%= post.title %></h2>
-    <h5><a href = "bulletins/<%= post.category %>/" style="text-decoration:none"><%= post.category %></a> </h4>
+    <h5><a href="/bulletins/<%= post.category %>/" style="text-decoration:none"><%= post.category %></a> </h4>
     <span>By  <a href="/profile/<%= post.author %>" style="text-decoration: none"><%= post.authorName %></a> | <%= (new Date(post.postTime)).toLocaleDateString(undefined, { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' }) %></span>
     <p id="post-text"><%= post.text %></p>
     
@@ -11,12 +11,12 @@ const post_template = `
                 <p><%= attachment %></p>
             <% }); %>
         <% } %>
-        
+         
         <button onclick="approvePost(<%= JSON.stringify(post) %>)">Approve Post</button>
     <% } if (post.approved && post.attachments != false && post.attachments != undefined) { %>
         <% post.attachments.forEach(function(attachment) { %>
             <% if (attachment.endsWith('.png') || attachment.endsWith('.jpg')) { %>
-                <img src="<%= attachment %>"/>
+                <img class="attachment" src="<%= attachment %>"/>
             <% } else { %>
                 <p><%= attachment %></p>
             <% } %>
