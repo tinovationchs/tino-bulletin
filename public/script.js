8a2b3721725563847bec438beb105c0b0db31d99
Shine Chang <shine00chang@gmail.com>
2023-02-13 18:05:29 -0800

    whoopsie fix

---
+++ public/script.js
@@ -0,0 +1,115 @@
+const loading_elem = document.getElementById("loading_post");
+const feed = document.getElementById("feed");
+let loading = false;
+let offset = 0;
+
+function reachedBottom () {
+    document.removeEventListener("scroll", onscroll);
+    document.getElementById("loadingPost").style.display = 'none';
+}
+
+const onscroll = e => {
+    if (!shouldLoad()) return;
+    if (loading) return;
+    loading = true;
+    console.log("Loading!");
+    
+    // Start Load
+    const amount = 5;
+    let params = {
+        amount: amount,
+        offset: offset,
+    };
+
+    fetchPosts(params)
+    .then(res => res.json())
+    .then(res => {
+        console.log(res);
+        // If reached bottom 
+        if (res.reachedBottom) {
+            reachedBottom();
+            return;
+        }
+
+        for (const post of res) {
+            console.log(post);
+            const elem = document.createElement("div");
+            elem.innerHTML = ejs.render(post_template, {
+                post: post,
+                admin_view: ADMIN_VIEW,
+            });
+            feed.appendChild(elem);
+        };
+        offset += res.length;
+        setTimeout(() => {
+            loading = false;
+            console.log("throttle done");
+            onscroll();
+        }, 1500);
+    })
+    .catch(err => {
+        console.log("error: ", err);
+        setTimeout(() => {
+            loading = false;
+            console.log("throttle done");
+            onscroll();
+        }, 1500);
+    });
+};
+
+function shouldLoad () {
+    const BUF = 200;
+    const lim = feed.clientHeight - window.innerHeight - BUF;
+    const pos = window.scrollY;
+    return pos > lim;
+}
+
+async function pinPost (post) {
+    const res = await fetch('/api/posts/pin', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({category: CATEGORY, key: post.key})
+    });
+    if (res.status != 200) {
+        alert("error on pin");
+    } else {
+        location.reload();
+    }
+}
+async function unpin () {
+    const res = await fetch('/api/posts/pin', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({category: CATEGORY, key: "xxx"})
+    });
+    if (res.status != 200) {
+        alert("error on pin");
+    } else {
+        location.reload();
+    }
+}
+
+async function approvePost(post) {
+    const res = await fetch('/api/posts/approve', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({category: post.category, key: post.key})
+    });
+    if (res.status != 200) {
+        alert("error on approval");
+    } else {
+        location.reload();
+    }
+}
+
+document.addEventListener("scroll", onscroll);
+setTimeout(onscroll, 1000);
